8b3731a77e0764bdd17cadf9b345f08eec79503c
Abdiwahab Mohamed <fahadfar69@gmail.com>
2023-02-27 21:29:46 +0300

    Reactjs With Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
