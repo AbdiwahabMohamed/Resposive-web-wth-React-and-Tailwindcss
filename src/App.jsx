1f8446adad02b52f09bdb968949c913c477b2ae7
Abdiwahab Mohamed <fahadfar69@gmail.com>
2023-03-01 13:11:20 +0300

    Analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import reactLogo from "./assets/react.svg";
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
+import Analytics from "./component/Analytics";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
