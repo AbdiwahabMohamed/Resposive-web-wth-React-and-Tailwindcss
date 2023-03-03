2215c329e24dcd2dec5128c9fab053279af6d478
Abdiwahab Mohamed <fahadfar69@gmail.com>
2023-03-03 13:22:56 +0300

    Added Cards component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import Analytics from "./component/Analytics";
 import NewsLetter from "./component/Newsletter";
+import Cards from "./component/Cards";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Analytics />
       <NewsLetter />
+      <Cards />
     </div>
   );
 }
